feed9447d879666cb4e79cc7469ebcbf83168402
Dias <said.ahmetov@list.ru>
2020-11-29 23:20:13 +0600

    feat: completed adaptive for index.html

--- js/main.js
+++ js/main.js
@@ -4,7 +4,19 @@ $(function () {
     dots: true,
     prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
     nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
-  });
+
+    
+  responsive: [
+    {
+      breakpoint: 969,
+      settings: {
+        arrows: false
+      }
+    },
+    
+  ]
+});
+  
 
   $('.tab').on('click', function (e) {
     e.preventDefault();
@@ -26,6 +38,37 @@ $(function () {
     slidesToScroll: 1,
     prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt=""></button>',
     nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>',
+    responsive: [
+    {
+      breakpoint: 1301,
+      settings: {
+        arrows: false,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 1201,
+      settings: {
+        slidesToShow: 3,
+        dots: true
+      } 
+    },
+    {
+      breakpoint: 870,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      } 
+    },
+    {
+      breakpoint: 590,
+      settings: {
+        slidesToShow: 1,
+        dots: true
+      } 
+    }
+    
+  ]
   });
 
   $('.filter-style').styler();
@@ -63,4 +106,8 @@ $(function () {
     $('.menu-mobile__list').toggleClass('menu-mobile__list--active')
   });
 
+  $('.footer__topdrop').on('click', function () {
+    $(this).next().slideToggle();
+    $(this).toggleClass('footer__topdrop--active');
+  });
 });
